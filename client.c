108172f5cef921a5db936e72b335012c1f082390
OfeliaRv <ofelyaar@code.edu.az>
2020-05-27 16:39:17 +0400

    Changed poem.h and client.c

--- client.c
+++ client.c
@@ -1,17 +1,28 @@
 // C program to implement sighup(), sigint()
 // and sigquit() signal functions
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <time.h>
 
-// function declaration
-void sigint();
-void sigquit();
-void choose_signal(int num);
-int r2();
+#include "poem.h"
+
+int access_queue()  // access queue using unique key
+{
+    key_t key = ftok ("/etc/passwd", 'E') ;
+    if (key == -1) perror ("ftok");
+    int poem_sig_id = msgget(key, 0);
+    if (poem_sig_id == -1) perror("msgget");
+
+    return poem_sig_id;
+}
+
+void write_value(int id, char* text)  // function to add message to the queue
+{
+    Poem p; 
+    int r;
+    p.poem_sig_type = 25;
+    strcpy(p.poem_text, text);
+
+    r = msgsnd (id , &p, sizeof p - sizeof p.poem_sig_type , 0);     // Data is placed on to a message queue
+    if (r == -1) perror("msgsnd");
+}
 
 int main()
 {
@@ -56,17 +67,18 @@ int main()
     }
     return 0;
 }
+
 void sigint()
 {
     signal(SIGINT, sigint); /* reset signal */
     printf("CHILD: I have received a SIGINT\n");
 }
+
 void sigquit()
 {
     printf("My DADDY has Killed me!!!\n");
     exit(0);
 }
-
 
 int r2()
 {
--- poem.h
+++ poem.h
@@ -9,13 +9,27 @@
 #include <signal.h> 
 #include <sys/types.h> 
 #include <unistd.h> 
+#include <time.h>
 
 #define MAX 1024
 
 typedef struct {
-    enum { japanese , western} poem_type;
+    enum { japanese , western } poem_type; // same with signal type for now
     char poem_text[MAX];
     char poem_author[MAX];
+    long poem_sig_type;
 } Poem;
+
+
+// function declaration in client.c
+void sigint();
+void sigquit();
+void choose_signal(int num);
+int r2();
+
+// function declaration in server.c
+int create_queue();
+void remove_queue(int id);
+Poem read_value (int id);
 
 #endif
