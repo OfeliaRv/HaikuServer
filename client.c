a4023893ccdf98db2aa3f67235d1422c20f6ed29
nigaryusifzada <nigaray@code.edu.az>
2020-05-27 16:10:36 +0400

    sigint sigquit

--- client.c
+++ client.c
@@ -1,47 +1,75 @@
-#include "poem.h"
-  
+// C program to implement sighup(), sigint()
+// and sigquit() signal functions
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
 
-void choose_signal();
-  
-void main() 
-{ 
-    int pid; 
-  
+// function declaration
+void sigint();
+void sigquit();
+void choose_signal(int num);
+int r2();
+
+int main()
+{
+    int pid;
+    int num;
     /* get child process */
-    if ((pid = fork()) < 0) { 
-        perror("fork"); 
-        exit(1); 
-    } 
-  
-    if (pid == 0) { /* child */
-        signal(SIGINT, sigint); 
-        signal(SIGQUIT, sigquit); 
-        for (;;) 
-            ; /* loop for ever */
-    } 
-  
-    else /* parent */
-    { /* pid hold id of child */
-  
-        sleep(3); /* pause for 3 secs */
-        printf("\nPARENT: sending SIGINT\n\n"); 
-        kill(pid, SIGINT); 
-  
-        sleep(3); /* pause for 3 secs */
-        printf("\nPARENT: sending SIGQUIT\n\n"); 
-        kill(pid, SIGQUIT); 
-        sleep(3); 
-    } 
-} 
-  
+    if ((pid = fork()) < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        if (pid == 0)
+        { /* child */
 
-void choose_signal(int number){
-    if (number = 1){
-        signal(SIGINT, sigint); /* reset signal */
-        printf("CHILD: I have received a SIGINT\n"); 
+            num = r2();
+            if (num == 0)
+            {
+                sigint();
+            }
+            else if (num == 1)
+            {
+                sigquit();
+            }
+        }
+        //  for (;;)
+        //     ; /* loop for ever */
+
+        else /* parent */
+        {    /* pid hold id of child */
+
+            sleep(1); /* pause for 1 sec */
+            printf("\nPARENT: sending SIGINT\n\n");
+            kill(pid, SIGINT);
+
+            sleep(1); /* pause for 1 sec */
+            printf("\nPARENT: sending SIGQUIT\n\n");
+            kill(pid, SIGQUIT);
+            sleep(1);
+        }
     }
-    else if (number = 0){
-        printf("My DADDY has Killed me!!!\n"); 
-        exit(0); 
-    }
+    return 0;
+}
+void sigint()
+{
+    signal(SIGINT, sigint); /* reset signal */
+    printf("CHILD: I have received a SIGINT\n");
+}
+void sigquit()
+{
+    printf("My DADDY has Killed me!!!\n");
+    exit(0);
+}
+
+
+int r2()
+{
+    srand(time(NULL));
+    return (rand() % 2);
 }
